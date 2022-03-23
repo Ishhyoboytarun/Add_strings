79df832ec2df3c63c55130455e8ba5b4669daeb2
Tarun Gusain <36428256+Ishhyoboytarun@users.noreply.github.com>
2022-03-23 21:11:07 +0530

    Update Add_strings.py
    Writing test cases and testing with assert statement

--- Add_strings.py
+++ Add_strings.py
@@ -80,12 +80,45 @@ def add(numbers):
 
     return sum 
 
+
+def testAddMethod():
+    #valid test cases
+    assert add(['1,2,3','4,5','6','7,8']) == 36
+    assert add(['//%','1%2%3','9%0']) == 15
+    assert add(['//[***]','1***2***3000***3']) == 6
+    assert add(['//[%%][**]','1**3%%2**2','7**4%%3%%9'])==31
+    print("all the valid test cases are passed successfully")
+
+
+    #uncomment the below lines to see the exception raised when invalid delimiters are given
+    '''
+    #invalid test cases
+    assert add(['1,2,3,','4,5','6','7,8']) == "invalid delimiter(s) found", "No need of extra delimiter at the last of one line"
+    assert add(['//%','1%2%3','9%%0']) == "invalid delimiter(s) found", "Invalid delimiter is given, Length is larger than the original delimiter"
+    assert add(['//[***]','1***2**3']) == "invalid delimiter(s) found", "Invalid delimiter is given, Length is smaller than the original delimiter"
+    '''
+
+
+    #uncomment the below lines to see the exception raised when negative values are given
+    '''
+    #valid test cases with negative values
+    assert add(['1,2,-3','4,-5','6','7,8']) != 36
+    assert add(['//%','1%2%3','-9%0']) != 15
+    assert add(['//[***]','-1***-2***-3']) != 6
+    assert add(['//[%%%][**]','-1**3%%2**2','7**-4%%3%-9'])!=31
+    '''
             
+        
 if __name__ == "__main__":
+    #unit testing
+    testAddMethod()
+
     #taking infinite input
+    #uncomment the below lines to give user input and check
+    """
     numbers = []
     while 1:
         num = input()
         if num is "": break
         numbers.append(num)
-    print(add(numbers))
+    print(add(numbers))"""
