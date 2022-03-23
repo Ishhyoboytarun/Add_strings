3610ade428b1ab8d62d03cfb7c5cedc388655796
Tarun Gusain <36428256+Ishhyoboytarun@users.noreply.github.com>
2022-03-23 20:34:50 +0530

    Update Add_strings.py
    Checking if delimiter(s) is/are given, 
    If yes, then filter out the delimiters and store them..
    Otherwise, proceed with default delimiter..

--- Add_strings.py
+++ Add_strings.py
@@ -1,3 +1,37 @@
+def findDelimiters(x):
+    #separating the total no. of delimiters given
+    delimiters = set()
+    i = 0    
+    while i<len(x):
+        delimiter=""
+        if x[i]=='[':
+            i+=1
+            while x[i]!=']':
+                delimiter+=x[i]
+                i+=1
+                
+        delimiters.add(delimiter)
+        i+=1
+        
+    return delimiters
+
+def add(numbers):
+    #base condition
+    if numbers == "": return 0
+
+    delimiters = set()
+    delimiters.add(",")
+    start_index = 0
+
+    #setting delimiter and starting index
+    if numbers[0][0:2]=="//":
+        if numbers[0][2]!="[":
+            delimiters = numbers[0][2:]
+        else:
+            delimiters = findDelimiters(numbers[0][2:])
+
+        start_index = 1
+        
 if __name__ == "__main__":
     #taking infinite input
     numbers = []
