759e6b937c1b47cb64dce439e98c507fd4354f22
Tarun Gusain <36428256+Ishhyoboytarun@users.noreply.github.com>
2022-03-24 17:20:57 +0530

    Create main.py
    Main function of the file

---
+++ main.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    #unit testing
+    testAddMethod()
+
+    #taking infinite input
+    #uncomment the below lines to give user input and check
+    """
+    numbers = []
+    while 1:
+        num = input()
+        if num is "": break
+        numbers.append(num)
+    print(add(numbers))"""
